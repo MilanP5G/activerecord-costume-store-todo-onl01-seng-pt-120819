4b29e1b98ed98a71a600b1079ca4c08cc20a66f4
Milan Parmar <milan_parmar@hotmail.co.uk>
2020-03-10 11:22:32 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -3,6 +3,9 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.2]
     create_table :haunted_houses do |t|
       t.string :name
       t.string :location
+      t.string :theme
+      t.integer :price
+      t.
     end 
   end 
 end 
